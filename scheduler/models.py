a404cefe7e5aef2f6d6b06f95862a8dda20cdbef
Darya Parygina <Parygina.Darya.2001@yandex.ru>
2024-05-22 00:52:28 +0300

    Refactor app logic

--- scheduler/models.py
+++ scheduler/models.py
@@ -2,20 +2,9 @@ import yougile
 import yougile.models as models
 
 
-class UnauthorizedError(Exception):
-    pass
-
-
-class ForbiddenError(Exception):
-    pass
-
-
-class HTTPError(Exception):
-    pass
-
-
-class Tasks:
-    """Representation of app logic"""
+class AppLogicModel:
+    def __init__(self):
+        self.token = ""
 
     def auth(self, login: str, password: str, company_name: str):
         """Authorize to YouGile
@@ -26,30 +15,24 @@ class Tasks:
         :type password: str
         :param company_name: Company name
         :type company_name: str
-        :raises UnauthorisedError: Response 401
-        :raises ForbiddenError: Response 403
-        :raises HTTPError: Response 429
-        :raises ValueError: No company with name `company_name`
+        :raises ValueError: Authorization error
         """
         model = models.AuthKeyController_companiesList(
             login=login, password=password, name=company_name
         )
         response = yougile.query(model)
-        status = response.status_code
-        if status == 401:
-            raise UnauthorizedError("Unauthorized")
-        elif status == 403:
-            raise ForbiddenError("Forbidden")
-        elif status == 429:
-            raise HTTPError("HTTP error")
+        if response.status_code != 200:
+            raise ValueError()
 
         companies = response.json()["content"]
         if len(companies) != 1:
-            raise ValueError(f"No company with name {company_name}")
+            raise ValueError()
         company_id = companies[0]["id"]
 
         model = models.AuthKeyController_create(
             login=login, password=password, companyId=company_id
         )
         response = yougile.query(model)
+        if response.status_code != 200:
+            raise ValueError()
         self.token = response.json()["key"]
